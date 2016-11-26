a99e2de7490aba08e7a6f32cd0e4177328369e81
Lars Veldscholte <lars@tuxplace.nl>
2016-11-26 02:51:36 +0100

    Stub event detail

--- app/views/EventView.py
+++ app/views/EventView.py
@@ -1,13 +1,20 @@
-from app.models import Event
-from lib.ResourceView import ResourceView
+from lib.ResourceView import ResourceView, bind_model
+from app.models import Event, Registration
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class EventView(ResourceView):
+class EventView(LoginRequiredMixin, ResourceView):
+	model = Event
+
 	def index(self, request):
 		events = Event.objects.all()
 
 		return render(request, 'app/event_list.html', {'events': events})
 
+	@bind_model
+	def show(self, request, event):
+		active_regs = Registration.objects.filter(event_id=event.pk, withdrawn_at=None)
+		withdrawn_regs = Registration.objects.filter(event_id=event.pk).exclude(withdrawn_at=None)
+
+		return render(request, 'app/event_detail.html', {'event': event, 'regs': active_regs | withdrawn_regs})
