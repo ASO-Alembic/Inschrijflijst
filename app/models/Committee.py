d6de386d44d7bdff5acaf64a558870edd8d33c1c
Lars Veldscholte <lars@tuxplace.nl>
2017-02-16 23:16:23 +0100

    Add members to Committee

--- app/models/Committee.py
+++ app/models/Committee.py
@@ -4,8 +4,9 @@ from django.conf import settings
 
 class Committee(models.Model):
 	name = models.CharField(max_length=25)
-	chairman = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, null=True, blank=True)
+	chairman = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, null=True, blank=True, related_name='chaired_committees')
 	email = models.EmailField()
+	members = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='in_committees')
 
 	def __str__(self):
 		return self.name
