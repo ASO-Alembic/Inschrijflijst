720619d137271cd24f15bc26c77ad53466f5f38d
Lars Veldscholte <lars@tuxplace.nl>
2017-02-17 19:58:23 +0100

    Extract LDAP attribute names as constants

--- app/services/LDAPService.py
+++ app/services/LDAPService.py
@@ -9,7 +9,11 @@ class LDAPService:
 	(not str) for some archaic reason.
 	"""
 	# Last part of all DNs
-	dc_suffix = 'DC=alembic,DC=utwente,DC=nl'
+	DC_SUFFIX = 'DC=alembic,DC=utwente,DC=nl'
+
+	# Attribute names
+	CHAIRMAN = 'salsaCommiteeChairman'
+	USERNAME = 'sAMAccountName'
 
 	def __init__(self, uri, bind_dn, bind_pw):
 		"""
@@ -30,8 +34,8 @@ class LDAPService:
 		Returns a list of dicts (CN, members, chairman) where members is a list of usernames.
 		"""
 		filter = '(objectclass=group)'
-		dn = 'OU=Commissies,OU=Groepen,{}'.format(self.dc_suffix)
-		attrs = ['cn', 'member', 'salsaCommiteeChairman']
+		dn = 'OU=Commissies,OU=Groepen,{}'.format(self.DC_SUFFIX)
+		attrs = ['cn', 'member', self.CHAIRMAN]
 
 		result = self.connection.search_s(dn, ldap.SCOPE_ONELEVEL, filter, attrs)
 
@@ -43,8 +47,8 @@ class LDAPService:
 		Return committees the user is in.
 		"""
 		filter = '(&(objectclass=group)(member={}))'.format(self.get_dn_from_username(username))
-		dn = 'OU=Commissies,OU=Groepen,{}'.format(self.dc_suffix)
-		attrs = ['cn', 'member', 'salsaCommiteeChairman']
+		dn = 'OU=Commissies,OU=Groepen,{}'.format(self.DC_SUFFIX)
+		attrs = ['cn', 'member', self.CHAIRMAN]
 
 		result = self.connection.search_s(dn, ldap.SCOPE_ONELEVEL, filter, attrs)
 
@@ -55,21 +59,21 @@ class LDAPService:
 		"""
 		Return username from a distinguished name.
 		"""
-		# Get sAMAccountName from DN
+		# Get USERNAME from DN
 		filter = '(objectclass=*)'
-		attrs = ['sAMAccountName']
+		attrs = [self.USERNAME]
 		result = self.connection.search_s(dn, ldap.SCOPE_BASE, filter, attrs)
 
-		# Above result is a list of tuples of the form (dn, attrs) where attrs is a dict containing sAMAccountName
+		# Above result is a list of tuples of the form (dn, attrs) where attrs is a dict containing USERNAME
 		# which is a list of a (byte) string, which we're interested in. Yeah.
-		return result[0][1]['sAMAccountName'][0].decode("utf-8")
+		return result[0][1][self.USERNAME][0].decode("utf-8")
 
 	def get_dn_from_username(self, username):
 		"""
 		Return distinguished name from username.
 		"""
-		filter = '(&(objectclass=person)(sAMAccountName={}))'.format(username)
-		dn = 'OU=Gebruikers,{}'.format(self.dc_suffix)
+		filter = '(&(objectclass=person)({}={}))'.format(self.USERNAME, username)
+		dn = 'OU=Gebruikers,{}'.format(self.DC_SUFFIX)
 		result = self.connection.search_s(dn, ldap.SCOPE_SUBTREE, filter, [])
 
 		return result[0][0]
@@ -79,8 +83,8 @@ class LDAPService:
 		Build a dict containing cn, members and chairman for a commitee from a dict of attributes returned from LDAP.
 		"""
 		# Lookup username for chairman. Some committees don't have this attribute.
-		if 'salsaCommiteeChairman' in attrs:
-			chairman = self.get_username_from_dn(attrs['salsaCommiteeChairman'][0].decode('utf-8'))
+		if self.CHAIRMAN in attrs:
+			chairman = self.get_username_from_dn(attrs[self.CHAIRMAN][0].decode('utf-8'))
 		else:
 			chairman = None
 
