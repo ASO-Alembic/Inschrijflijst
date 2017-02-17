9975140db3ceae87f3dad7a639769b64a8764a15
Lars Veldscholte <lars@tuxplace.nl>
2017-02-17 15:21:13 +0100

    Add get_in_committees(), some refactoring

--- app/services/LDAPService.py
+++ app/services/LDAPService.py
@@ -35,28 +35,21 @@ class LDAPService:
 
 		result = self.connection.search_s(dn, ldap.SCOPE_ONELEVEL, filter, attrs)
 
-		# Create and return list of dicts from LDAP query result
-		committee_list = []
-		for dn, attrs in result:
-			# Lookup username for chairman. Some committees don't have this attribute.
-			if 'salsaCommiteeChairman' in attrs:
-				chairman = self.get_username_from_dn(attrs['salsaCommiteeChairman'][0].decode('utf-8'))
-			else:
-				chairman = None
+		# Build and return list of dicts from LDAP query result
+		return [self.build_committee_dict(attrs) for dn, attrs in result]
 
-			# For every member, lookup the username. Some committees don't have this attribute.
-			if 'member' in attrs:
-				members = [self.get_username_from_dn(m.decode('utf-8')) for m in attrs['member']]
-			else:
-				members = []
+	def get_in_committees(self, username):
+		"""
+		Return committees the user is in.
+		"""
+		filter = '(&(objectclass=group)(member={}))'.format(self.get_dn_from_username(username))
+		dn = 'OU=Commissies,OU=Groepen,{}'.format(self.dc_suffix)
+		attrs = ['cn', 'member', 'salsaCommiteeChairman']
 
-			committee_list.append({
-				'cn': attrs['cn'][0].decode("utf-8"),
-				'members': members,
-				'chairman': chairman
-			})
+		result = self.connection.search_s(dn, ldap.SCOPE_ONELEVEL, filter, attrs)
 
-		return committee_list
+		# Build and return list of dicts from LDAP query result
+		return [self.build_committee_dict(attrs) for dn, attrs in result]
 
 	def get_username_from_dn(self, dn):
 		"""
@@ -70,3 +63,35 @@ class LDAPService:
 		# Above result is a list of tuples of the form (dn, attrs) where attrs is a dict containing sAMAccountName
 		# which is a list of a (byte) string, which we're interested in. Yeah.
 		return result[0][1]['sAMAccountName'][0].decode("utf-8")
+
+	def get_dn_from_username(self, username):
+		"""
+		Return distinguished name from username.
+		"""
+		filter = '(&(objectclass=person)(sAMAccountName={}))'.format(username)
+		dn = 'OU=Gebruikers,{}'.format(self.dc_suffix)
+		result = self.connection.search_s(dn, ldap.SCOPE_SUBTREE, filter, [])
+
+		return result[0][0]
+
+	def build_committee_dict(self, attrs):
+		"""
+		Build a dict containing cn, members and chairman for a commitee from a dict of attributes returned from LDAP.
+		"""
+		# Lookup username for chairman. Some committees don't have this attribute.
+		if 'salsaCommiteeChairman' in attrs:
+			chairman = self.get_username_from_dn(attrs['salsaCommiteeChairman'][0].decode('utf-8'))
+		else:
+			chairman = None
+
+		# For every member, lookup the username. Some committees don't have this attribute.
+		if 'member' in attrs:
+			members = [self.get_username_from_dn(m.decode('utf-8')) for m in attrs['member']]
+		else:
+			members = []
+
+		return {
+			'cn': attrs['cn'][0].decode("utf-8"),
+			'members': members,
+			'chairman': chairman
+		}
