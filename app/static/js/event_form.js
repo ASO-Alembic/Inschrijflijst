2ba2c750a2bcd1a98f8458a4bbe93c200cfcbc66
Lars Veldscholte <lars@tuxplace.nl>
2017-08-01 18:55:16 +0200

    Use readonly instead of disabled state for inputs, some refactoring
    Fixes bug where disabled inputs are not POSTed

--- app/static/js/event_form.js
+++ app/static/js/event_form.js
@@ -3,27 +3,31 @@
  */
 
 $(document).ready(function () {
-	// Initial check at load for fieldset
 	$('#note_field_check').click(setNoteFieldsetState);
 
-	// Set disabled state of tagsinput according to wether its empty or not
+	// Initial check at load for fieldset
+	setNoteFieldsetState();
+
+	// Set readonly state of tagsinput according to wether its empty or not (for filled form)
 	if($('#id_note_field_options').val() == '')
-		$('.bootstrap-tagsinput input').prop('disabled', true);
+		$('.bootstrap-tagsinput input').prop('readonly', true);
 
 	// Disable and clear tagsinput on radio change
 	$('input:radio').change(function() {
-		$(this).closest('.form-group').find("#id_note_field_options").tagsinput('removeAll');
-		$(this).closest('.form-group').find("input:text").prop('disabled', true);
+		$("#id_note_field_options").tagsinput('removeAll');
+		$('.bootstrap-tagsinput input').prop('readonly', true);
 
 		if ($(this).is(":checked"))
-			$(this).parent().parent().parent().next().find("input:text").prop('disabled', false);
+			$(this).parent().parent().parent().next().find("input:text").prop('readonly', false);
 	});
 });
 
-// Set disabled state of fieldset according to checked state of checkbox
+// Set readonly state of fieldset according to checked state of checkbox
 function setNoteFieldsetState() {
-	$('#note_fieldset').prop('disabled', !$('#note_field_check').is(':checked'));
+	var checked = $('#note_field_check').is(':checked');
 
-	if(!$('#note_field_check').is(':checked'))
+	$('#note_fieldset input').prop('readonly', !checked);
+
+	if(!checked)
 		$('#id_note_field').val('')
 }
