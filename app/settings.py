ed7e1f36d2c483bebe0ae16d71f6979bda6c39bc
Lars Veldscholte <lars@tuxplace.nl>
2017-09-16 01:45:42 +0200

    Use global/static formats
    ISO 8601 or GTFO

--- app/settings.py
+++ app/settings.py
@@ -99,17 +99,13 @@ STATIC_URL = FORCE_SCRIPT_NAME + '/static/'
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 USE_I18N = True
-USE_L10N = True
+USE_L10N = False
 USE_TZ = True
 LANGUAGE_CODE = 'NL-nl'
 
 LANGUAGES = [
 	('nl', _('Dutch')),
 	('en', _('English')),
-]
-
-FORMAT_MODULE_PATH = [
-	'app.formats',
 ]
 
 MESSAGE_TAGS = {
@@ -126,3 +122,30 @@ EMAIL_BACKEND = 'post_office.EmailBackend'
 POST_OFFICE = {
 	'BATCH_SIZE': 30
 }
+
+# ISO 8601 or GTFO
+DATE_FORMAT = 'j F Y'  # '20 januari 2009'
+TIME_FORMAT = 'H:i'  # '15:23'
+DATETIME_FORMAT = 'j F Y H:i'  # '20 januari 2009 15:23'
+MONTH_DAY_FORMAT = 'j F'  # '20 januari'
+SHORT_DATE_FORMAT = 'Y-n-j'  # '2009-1-20'
+SHORT_DATETIME_FORMAT = 'Y-n-j H:i'  # '2009-1-20 15:23'
+FIRST_DAY_OF_WEEK = 1  # Monday
+
+DATE_INPUT_FORMATS = [
+	'%Y-%m-%d',  # '2009-1-20'
+	'%d-%m-%Y'  # '20-01-2009'
+]
+TIME_INPUT_FORMATS = [
+	'%H:%M:%S',  # '15:23:35'
+	'%H:%M:%S.%f',  # '15:23:35.000200'
+	'%H:%M'  # '15:23'
+]
+DATETIME_INPUT_FORMATS = [
+	'%Y-%m-%d %H:%M:%S',  # '2006-10-25 14:30:59'
+	'%Y-%m-%d %H:%M:%S.%f',  # '2006-10-25 14:30:59.000200'
+	'%Y-%m-%d %H:%M',  # '2006-10-25 14:30'
+	'%Y-%m-%d'  # '2006-10-25'
+]
+
+THOUSAND_SEPARATOR = ' '
