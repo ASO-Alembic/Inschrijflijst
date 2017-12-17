29ded850e3330a451024a8a2eb75c486b7fe23d8
Lars Veldscholte <lars@tuxplace.nl>
2017-12-17 02:16:46 +0100

    Use new url patterns

--- app/urls.py
+++ app/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.urls import path, re_path, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.shortcuts import redirect, render
@@ -29,20 +29,20 @@ router.register(['committees'], CommitteeView, 'committee')
 router.register(['events', 'mail'], MailView, 'mail')
 
 urlpatterns = [
-	url(r'^$', lambda request: redirect('event-list'), name='home'),
-	url(r'^faq$', lambda request: render(request, 'faq.html'), name='faq'),
-	url(r'^calendar$', CalendarView.as_view('show'), name='calendar'),
-	url(r'^admin$', AdminView.as_view('show'), name='admin'),
-	url(r'^admin/sync-ldap$', AdminView.as_view('sync-ldap'), name='admin-sync-ldap'),
-	url(r'^admin/calendar$', AdminView.as_view('calendar'), name='admin-calendar'),
-	url(r'^admin/calendar/flow$', AdminView.as_view('calendar-flow'), name='admin-calendar-flow'),
+	path('', lambda request: redirect('event-list'), name='home'),
+	path('faq', lambda request: render(request, 'faq.html'), name='faq'),
+	path('calendar', CalendarView.as_view('show'), name='calendar'),
+	path('admin', AdminView.as_view('show'), name='admin'),
+	path('admin/sync-ldap', AdminView.as_view('sync-ldap'), name='admin-sync-ldap'),
+	path('admin/calendar', AdminView.as_view('calendar'), name='admin-calendar'),
+	path('admin/calendar/flow', AdminView.as_view('calendar-flow'), name='admin-calendar-flow'),
 
-	url(r'^api/stats/events/(\d+)/registrations/$', StatsView.as_view('registrations'), name='api-stats-registrations'),
+	re_path(r'^api/stats/events/(\d+)/registrations/$', StatsView.as_view('registrations'), name='api-stats-registrations'),
 
-	url(r'^django-admin/', admin.site.urls, name='django-admin'),
-	url(r'^login/$', auth_views.login, name='login'),
-	url(r'^logout/$', auth_views.logout, name='logout'),
-	url(r'^i18n/', include('django.conf.urls.i18n'))
+	path('django-admin/', admin.site.urls, name='django-admin'),
+	path('login/', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
+	path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+	path('i18n/', include('django.conf.urls.i18n'))
 ]
 
 urlpatterns.extend(router.urls())
