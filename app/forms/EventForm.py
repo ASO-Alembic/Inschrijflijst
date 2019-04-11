f2338f1945b8ec788a2034fd7dfb4d25dbd6388c
Trance <walliewhal4@gmail.com>
2019-04-11 19:21:18 +0200

    Add FCM notification support
    This should make it possible to create notifications for android app users.

--- app/forms/EventForm.py
+++ app/forms/EventForm.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from bootstrap3_datetime.widgets import DateTimePicker
 
 from app.models import Event
-from app.services import GoogleCalendarService
+from app.services import GoogleCalendarService, FCMService
 from lib.Mail import Mailer
 
 
@@ -14,10 +14,11 @@ class EventForm(forms.ModelForm):
 
 		self.fields['committee'].queryset = user.get_admined_committees()
 
-		# Only show add_to_calendar and enroll_committee fields when creating new event
+		# Only show add_to_calendar, enroll_committee and send_notification fields when creating new event
 		if instance is not None:
 			self.fields.pop('add_to_calendar')
 			self.fields.pop('enroll_committee')
+			self.fields.pop('send_notification')
 
 	class Meta:
 		model = Event
@@ -65,6 +66,7 @@ class EventForm(forms.ModelForm):
 
 	add_to_calendar = forms.BooleanField(required=False, initial=True, label=_("Toevoegen aan jaarcirkel"))
 	enroll_committee = forms.BooleanField(required=False, initial=False, label=_("Schrijf je commissie in"))
+	send_notification = forms.BooleanField(required=False, initial=True, label=_("Stuur notificatie naar app gebruikers") )
 
 	def save(self, request, commit=True):
 		# The request object is needed for two things here: the base_url and the email message
@@ -80,6 +82,15 @@ class EventForm(forms.ModelForm):
 				event.save()
 			except RuntimeError:
 				# Calendar not set up
+				pass
+
+		# Send notification to app users
+		if self.cleaned_data.get('send_notification'):
+			try:
+				fcm_service = FCMService(request.base_url)
+				fcm_service.notify(event)
+			except RuntimeError:
+				# Notification not sent
 				pass
 
 		if self.cleaned_data.get('enroll_committee'):
