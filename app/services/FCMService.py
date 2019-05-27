b87a97aad1213d3eb417b0ad1268e91b358044d2
Lars Veldscholte <lars@tuxplace.nl>
2019-05-27 22:20:32 +0200

    Fix syntax errors

--- app/services/FCMService.py
+++ app/services/FCMService.py
@@ -2,33 +2,33 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import messaging
 
-class FCMService:
-    """
-    Service for communicating with Firebase Cloud Messaging
-    """
-    FCM_CLIENT_SECRET_FILE = 'storage/FCM_client_id.json'
-    DEFAULT_TOPIC = 'all'
 
-    def __init__(self, base_url):
-        """
+class FCMService:
+	"""
+	Service for communicating with Firebase Cloud Messaging
+	"""
+	FCM_CLIENT_SECRET_FILE = 'storage/FCM_client_id.json'
+	DEFAULT_TOPIC = 'all'
+
+	def __init__(self, base_url):
+		"""
         Get credentials
         """
-        self.base_url = base_url
-        self.cred = credentials.Certificate(self.FCM_CLIENT_SECRET_FILE)
-        self.firebase_admin.initialize_app(self.cred)
+		self.base_url = base_url
+		cred = credentials.Certificate(self.FCM_CLIENT_SECRET_FILE)
+		self.app = firebase_admin.initialize_app(cred)
 
-    def notify(self, app_event):
-        """
-        Send notification to app users
-        """
-        message = messaging.Message(
+	def notify(self, app_event):
+		"""
+		Send notification to app users
+		"""
+		message = messaging.Message(
+			data={
+				'title': app_event.name,
+				'body': app_event.description,
+				'eventURL': self.base_url + app_event.get_absolute_url(),
+			},
+			topic=self.DEFAULT_TOPIC
+		)
 
-            data = {
-                'title': app_event.name,
-                'body': app_event.description,
-                'eventURL': self.base_url + app_event.get_absolute_url,
-            },
-            topic = self.DEFAULT_TOPIC,
-        )
-
-        messaging.send(message)
+		messaging.send(message)
