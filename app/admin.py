7827ebcbd964b03b409d055ef501870e08bdbbea
Trance <walliewhal4@gmail.com>
2021-03-15 17:14:51 +0100

    Improve searchability of users in the admin menu.

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Committee, Event, Registration, User
 
-admin.site.register([Committee, Event, Registration, User])
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'last_seen_at', 'is_superuser', 'is_staff')
+    search_fields = ['username']
+
+admin.site.register([Committee, Event, Registration])
